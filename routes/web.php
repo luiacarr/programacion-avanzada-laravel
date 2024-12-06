06afd927e5b3d8497fa5c4362ac4a090f81655d8
Luis Carrasco <cluis0152@gmail.com>
2024-12-05 22:26:41 -0500

    sesion 1

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\PostController;
 
 
 Route::get('/', function () {
@@ -25,7 +24,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::resource('posts', PostController::class);
 
 require __DIR__.'/auth.php';
