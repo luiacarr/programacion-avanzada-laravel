c7371e37b7105e4340852cf41d159ad5e74e9ad6
gariasojedis <gariasojedis@gmail.com>
2024-12-13 15:04:42 -0500

    inicio websocket

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,50 @@
 <?php
 
+use App\Models\ChatMessage;
+use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Foundation\Application;
+use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Pest\Support\View;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return view('Welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+Route::get('/dashboard', function () {
+    return View('dashboard',['users' => User::whereNo('id',auth()->id())->get()]);
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/chat/{fried}', function (User $friend) {
+    return View('chat',['friend' => $friend]);
+})->middleware(['auth'])->name('chat');
+
+Route::get('/messages/{fried}', function (User $friend) {
+    return ChatMessage::query()
+    ->where(function($query) use ($friend){
+        $query->where('sender_id',auth()->id())
+        ->where('receiver_id',$friend->id);
+    })
+    ->where(function($query)use ($friend){
+        $query->where('sender_id',$friend->id())
+        ->where('receiver_id', auth()->id());
+    })
+    ->with(['sender','receiver'])
+    ->orderBy('id','asc')
+    ->get();
+})->middleware(['auth']);
+
+Route::get('/messages/{fried}', function (User $friend) {
+    $message=ChatMessage::create([
+        'sender_id'=>auth()->id(),
+         'receiver_id'=>$friend->id,
+        'text'=>request()->imput('message')
+    ]);
+    broadcast(new Message($message));
+    return $message;
 });
+
+
+require __DIR__.'/auth.php';
---
+++ themes/tailwind/js/components/ChatComponent.vue
@@ -0,0 +1,40 @@
+<template>
+<div> 
+    <div class="flex flex-col justify-end h-80">
+        <div ref="messageContainer" class="p-4 overflow-y-auto max-h-fit">
+            <div
+                v-for="message in messages"
+                :key="message.id"
+                class="flex items-center mb-2"
+            >
+                <div
+                    v-if="message.sender_id === currentUser.id"
+                    class="p-2 mr-auto bg-gray-200 rounded-lg"
+                >
+                    {{ message.text }}
+                </div>  
+                <div v-else class="p-2 mr-auto bg-gray-200 rounded-lg">
+                    {{ message.text }}
+                </div>
+            </div>
+        </div>
+    </div> 
+</div>
+<div class="flex items-center">
+    <input
+        type="text"
+        v-model="newMesage"
+        @keydown="sendTypingEvent"
+        @keyup.enter="sendMessage"
+        placeholder="Type a message...."
+        class="flex-1 px-2 py-1 border rounded-lg"
+    >
+    <button
+    @click="sendMessage"
+    class="px-4 py-1 ml-2 text-white bblue-500 ounded-lg"
+    >
+    Send
+    </button>
+</div>
+
+ 
